cbabfa6818fdaf35d3e800af4e2f7ab862d87bee
Bryan <bryaument@gmail.com>
2019-03-07 01:29:00 -0500

    added getters and setters for the origin, also added tuple like class

--- src/main/java/storming/uml_editor/model/things/UML_Thing.java
+++ src/main/java/storming/uml_editor/model/things/UML_Thing.java
@@ -2,10 +2,46 @@ package storming.uml_editor.model.things;
 
 import storming.uml_editor.model.UML_Element;
 
-/**
- * A class that represents a UML Thing
- */
+
 public abstract class UML_Thing extends UML_Element {
+	
+	/**
+	 * Defines the origin of all UML_Thing(s) as X and Y 
+	 * @author bryan
+	 *
+	 * @param <X>
+	 * @param <Y>
+	 * 
+	 */
+	 private class Origin<X, Y> {
+		private final X xCoordinate; 
+		private final Y yCoordinate; 
+		
+		public Origin(X xCoordinate, Y yCoordinate) {
+			this.xCoordinate = xCoordinate;
+			this.yCoordinate = yCoordinate;
+		}
+		
+		protected final X getX() {
+			return xCoordinate; 
+		}
+		
+		protected final Y getY() {
+			return yCoordinate;
+		}	
+	}
+	
+	 //TODO Need to be able to get x and y coordinates from UML_Controller
+	 private Origin<?, ?> UML_ThingOrigin = null;
+
+	 public Origin<?, ?> getXCoordinate() {
+		return (Origin<?, ?>) UML_ThingOrigin.getX();
+	 }
+	 
+	 public Origin<?, ?> getYCoordinate() {
+			return (Origin<?, ?>) UML_ThingOrigin.getY();
+	 }
+	 
 	/**
 	 * The default constructor for a UML Thing
 	 */
@@ -21,4 +57,7 @@ public abstract class UML_Thing extends UML_Element {
 	protected UML_Thing(String name) {
 		super(name);
 	}
+
+	
+	
 }
