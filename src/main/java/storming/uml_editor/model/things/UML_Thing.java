60915e2fa7f748f71e5ed8812d0f541401a80dc6
Bryan <bryaument@gmail.com>
2019-03-07 12:17:26 -0500

    updated getters and setters to model 'member attributes' rather than a class

--- src/main/java/storming/uml_editor/model/things/UML_Thing.java
+++ src/main/java/storming/uml_editor/model/things/UML_Thing.java
@@ -2,51 +2,33 @@ package storming.uml_editor.model.things;
 
 import storming.uml_editor.model.UML_Element;
 
-
 public abstract class UML_Thing extends UML_Element {
+
+	// members variables
+	private double xCoordinate = 0.0;
+	private double yCoordinate = 0.0;
 	
-	/**
-	 * Defines the origin of all UML_Thing(s) as X and Y 
-	 * @author bryan
-	 *
-	 * @param <X>
-	 * @param <Y>
-	 * 
-	 */
-	 private class Origin<X, Y> {
-		private final X xCoordinate; 
-		private final Y yCoordinate; 
-		
-		public Origin(X xCoordinate, Y yCoordinate) {
-			this.xCoordinate = xCoordinate;
-			this.yCoordinate = yCoordinate;
-		}
-		
-		protected final X getX() {
-			return xCoordinate; 
-		}
-		
-		protected final Y getY() {
-			return yCoordinate;
-		}	
+	public void setX(double x) {
+		this.xCoordinate = x;
 	}
 	
-	 //TODO Need to be able to get x and y coordinates from UML_Controller
-	 private Origin<?, ?> UML_ThingOrigin = null;
-
-	 public Origin<?, ?> getXCoordinate() {
-		return (Origin<?, ?>) UML_ThingOrigin.getX();
-	 }
-	 
-	 public Origin<?, ?> getYCoordinate() {
-			return (Origin<?, ?>) UML_ThingOrigin.getY();
-	 }
-	 
+	public void setY(double y) {
+		this.yCoordinate = y; 
+	}
+	
+	public double getX() {
+		return this.xCoordinate;
+	}
+	
+	public double getY() {
+		return this.yCoordinate; 
+	}
+	
 	/**
 	 * The default constructor for a UML Thing
 	 */
 	protected UML_Thing() {
-		super();
+		super();	
 	}
 	
 	/**
@@ -57,7 +39,4 @@ public abstract class UML_Thing extends UML_Element {
 	protected UML_Thing(String name) {
 		super(name);
 	}
-
-	
-	
 }
