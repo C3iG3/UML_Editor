79101e3f5b4fed8a1e0ca32ec62d04c532f65a87
Bryan <bryaument@gmail.com>
2019-03-16 18:39:50 -0400

    added simple JUnit test file for model - as Dr. H's request

---
+++ src/test/java/storming/uml_editor/model/UML_ModelTest.java
@@ -0,0 +1,38 @@
+/*
+ * NOTE: Added to the build path so there could be some issues. 
+ * 
+ * 
+ * 
+ */
+
+
+package storming.uml_editor.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UML_ModelTest {
+
+	@Test
+	void testUML_Model() {
+		UML_Model mod = new UML_Model(null); 
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
