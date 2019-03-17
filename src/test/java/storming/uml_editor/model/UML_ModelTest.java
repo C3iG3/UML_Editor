8949a2d933fba5beb9b488aa672d699841a90032
Bryan <bryaument@gmail.com>
2019-03-17 15:24:20 -0400

    added some basic test skeleton code

--- src/test/java/storming/uml_editor/model/UML_ModelTest.java
+++ src/test/java/storming/uml_editor/model/UML_ModelTest.java
@@ -13,26 +13,33 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class UML_ModelTest {
-
+	
+	/**
+	 * Description: Testing newly constructed model. 
+	 */
 	@Test
 	void testUML_Model() {
-		UML_Model mod = new UML_Model(null); 
-		fail("Not yet implemented");
+		UML_Model emptyModel = new UML_Model(null); 
+		// 0 is the instance of the first (key:value)
+		assertTrue(emptyModel.get(0)== null, "There should be no elements in the hashmap");
+		
 	}
 
-	@Test
-	void testGet() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	void testPut() {
-		fail("Not yet implemented");
+		UML_Model putModel = new UML_Model(null);
 	}
+	
+	@Test
+	void testGet() {
+		UML_Model getModel = new UML_Model(null);
+	}
+
 
 	@Test
 	void testRemove() {
-		fail("Not yet implemented");
+		UML_Model removeModel = new UML_Model(null);
 	}
 
 }
